40162e323cf2e6e4597960abd9a6e37973ecfdeb
Al-Dos024 <amjdos0amjdos0190edu@gmail.com>
2024-02-19 22:21:24 +0200

    Done with update need to be sorted (dictionary of dictionary)

--- Dos.py
+++ Dos.py
@@ -1,24 +1,24 @@
 students = [
-    {'name': 'John', 'age': 18, 'grade': 'A', 'id': 1},
-    {'name': 'Emma', 'age': 17, 'grade': 'B', 'id': 2},
-    {'name': 'Michael', 'age': 16, 'grade': 'C', 'id': 3}
+    {'id': 1 ,'name':'John', 'age': 18, 'grade': 'A'},
+    {'id': 2 ,'name':'Emma', 'age': 17, 'grade': 'B'},
+    {'id': 3 ,'name':'Michael', 'age': 16, 'grade': 'C'}
 ]
 
 def view_students():
     if len(students) != 0:
         for student in students:
             # printing students and ding some foramat
-            print(f"{students.index(student) + 1}- Name:{student['name']:<10}"
-                  f"Age:{student['age']:<5}Grade:{student['grade']:<5}ID:{student['id']:<5}")
+            print(f"{students.index(student) + 1}- ID:{student['id']:<5} Name:{student['name']:<10}"
+                  f"Age:{student['age']:<5}Grade:{student['grade']:<5}")
     else:
         print("No students to print! ")
 
 
-# the update is not working yet
+# if sorted it work , we need to make it dictionary of dic
 def update_student_details(name, student_id, grade, age):
-    students[student_id] = {   
+    students[student_id - 1] = {   
             'name': name,
-            'id': student_id,
+            'id' : student_id,
             'grade': grade,
             'age': age,
         }
@@ -35,6 +35,8 @@ def delete_student(id):
     if not found:
         print("This ID not found")
 
+# def styling ():
+
 
 while True:
     print("Welcome to Student Database Management System")
@@ -47,6 +49,7 @@ while True:
     print("6 - Save to File ")
     print("7 - Load from File")
     print("press (-1) to Exit")
+
     try:
         choice = int(input("Enter your choice:"))
         if choice == -1:
@@ -58,16 +61,18 @@ while True:
             print("------------------------------------")
 
         elif choice == 4:
+            found = False
             student_id = int(input("Enter student ID to update details: "))
             for student in students:    
-                if student['id'] in student_id :
+                if student['id'] == student_id :
                     name = input("Enter student name: ")
                     grade = input("Enter student grade: ")
                     age = int(input("Enter student age: "))
                     update_student_details(name, student_id, grade, age)
-                    print("Student details updated successfully.")
-                else:
-                    print("Student not found.")
+                    found = True
+                    break
+            if not found:
+                print("Student not found.")
         
         elif choice == 5:
             your_id = int(input("Enter id you want to delete?"))
@@ -75,5 +80,5 @@ while True:
         else:
             print("you input is wrong!!")
     except ValueError:
-        print(" you input is wrong!!")
+        print("you input is wrong!!")
 
