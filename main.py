06f48ac389c267fc28f599b87d388c333f98de39
Ali Dosoqi <106039568+AlDos024@users.noreply.github.com>
2024-02-20 23:57:33 +0200

    Shahd branch add fun (#4)
    * update
    
    * new
    
    * new function
    
    * final add function
    
    ---------
    
    Co-authored-by: shahdnass <123496373+shahdnass@users.noreply.github.com>

--- main.py
+++ main.py
@@ -3,11 +3,88 @@ import os
 id = 0
 last_loaded_id = 0
 loaded_students = {}
-students = {
-    1: {"first_name": "Amr",   "last_name":"ali","email":"amr@gmail.com",    "age": 19, "gpa": 3.79,"address":"Cairo"},
-    2: {"first_name": "Hossam","last_name":"ali","email":"ali@gmail.com",    "age": 20, "gpa": 2.5 ,"address":"Tokyo"},
-    3: {"first_name": "Ali",   "last_name":"ali","email":"hossam@gmail.com", "age": 20, "gpa": 3.07,"address":"madrid"},
-}
+students = {}
+def add_students():
+
+    def add_student():
+        while True:
+            first_name = input("Enter student first name: ")
+            if first_name.isalpha():
+                break
+            print("Invalid input")
+
+        while True:
+            last_name = input("Enter student last name: ")
+            if last_name.isalpha():
+                break
+            print("Invalid input")
+
+        while True:
+            email = input("Enter student email: ")
+            if email.strip() and '@' in email:
+                break
+            print("Invalid email address format.")
+
+        while True:
+            gender = input("Enter student gender (Male/Female): ").lower()
+            if gender in ['male', 'female']:
+                break
+            print("Gender must be either 'Male' or 'Female'.")
+
+        while True:
+            try:
+                age = int(input("Enter student age as an integer from 6 to 100: "))
+                if 6 <= age <= 100:
+                    break
+                print("Invalid input. Please enter a value between 6 and 100.")
+            except ValueError:
+                print("Invalid input")
+
+        address = input("Enter student address: ")
+        while not address.strip():
+            print("Address cannot be empty.")
+            address = input("Enter student address: ")
+
+        while True:
+            try:
+                gpa = float(input("Enter student grade: "))
+                if 0 <= gpa <= 4:
+                    break
+                print("Invalid input. Please enter a value between 0 and 4.")
+            except ValueError:
+                print("Invalid input.")
+
+        nonlocal students
+        student_id = str(len(students) + 1)
+        students[student_id] = {
+            'first_name': first_name,
+            'last_name': last_name,
+            'email': email,
+            'gender': gender,
+            'age': age,
+            'address': address,
+            'gpa': gpa
+        }
+
+    num_students = int(input("How many students do you want to add? "))
+
+    for _ in range(num_students):
+        add_student()
+
+    print("Students successfully added!")
+    return students
+
+# def display_students_data(students):
+#     for student_id, data in students.items():
+#         print(f"Student ID: {student_id}")
+#         print(f"First Name: {data['first_name']}")
+#         print(f"Last Name: {data['last_name']}")
+#         print(f"Email: {data['email']}")
+#         print(f"Gender: {data['gender']}")
+#         print(f"Age: {data['age']}")
+#         print(f"Address: {data['address']}")
+#         print(f"GPA: {data['gpa']}")
+#         print()
 
 def view_students():
     index = 0
@@ -129,7 +206,9 @@ while True:
         choice = int(input("Enter your choice:"))
         if choice == -1:
             break
-
+        elif choice == 1:
+          students = add_students()
+#           display_students_data(students)
         elif choice == 2:
             print("------------------------------------")
             view_students()
