a20134ead3e15bee0a745482b1969368d0c25e40
Ali Dosoqi <106039568+AlDos024@users.noreply.github.com>
2024-02-21 13:59:01 +0200

    Marwan's branch (#6)
    * Incomplete
    
    * dd
    
    * used json
    
    * used json
    
    * save and load
    
    * load is'nt done yet
    
    * done
    
    * done
    
    * done
    
    * done
    
    * View
    
    * load
    
    * id problem fixed
    
    * fixed a few bugs
    
    * fixed a few bugs
    
    * update is now working + save appends new data to old data or rewrites data if old data is changed
    
    ---------
    
    Co-authored-by: Marwan Walid <87389616+Marwan-alanani@users.noreply.github.com>
    Co-authored-by: Marwan-alanani <marwan-walid1@hotmail.com>

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ students = {}
 overwrite = False
 loaded = False
 
+
 # to not face errors in saving and loading
 def removeChars(element):
     element = element.replace("/", "")
     element = element.replace(";", "")
     element = element.replace(":", "")
     return element
+
 
 def get_first_name():
     while True:
@@ -79,6 +81,7 @@ def get_gpa():
         except ValueError:
             print("Invalid input.")
 
+
 def add_students():
 
     def add_student():
@@ -117,29 +120,32 @@ def add_students():
 
     print("Student(s) added successfully !")
 
+
+
+def search_student(id):
+    if students.get(id):
+        print(
+            f"ID:{id:<4} Name :{students.get(id)['first_name']}{students.get(id)['last_name']:<10}Email :{students.get(id)['email']:<20}"
+            f"Age :{students.get(id)['age']:<7}Gpa :{students.get(id)['gpa']:<6} gender :{students.get(id)['gender']:<12} Address :{students.get(id)['address']},gender:{students.get(id)['gender']:<10}"
+        )
+    else:
+        print("Student not found")
+
 #no change
 def view_students():
-    index = 0
+    index = 1
     if len(students) != 0:
         for key, value in students.items():
             # printing students and ding some foramat
             print(
-                f"{index + 1}- ID:{key:<4} Name :{value['first_name']} {value['last_name']:<10} Email :{value['email']:<30}"
+                f"{index}- ID:{key:<4} Name :{value['first_name']} {value['last_name']:<10} Email :{value['email']:<30}"
                 f" Age :{value['age']:<7} Gpa :{value['gpa']:<8} gender :{value['gender']:<6} Address :{value['address']}"
             )
             index += 1
     else:
         print("No students to print! ")
 
-#no change
-def search_student(id):
-    if students.get(id) != None:
-        print(
-            f"ID:{id:<4} Name :{students.get(id)['first_name']}{students.get(id)['last_name']:<10}Email :{students.get(id)['email']:<20}"
-            f"Age :{students.get(id)['age']:<7}Gpa :{students.get(id)['gpa']:<6} gender :{students.get(id)['gender']:<12} Address :{students.get(id)['address']},gender:{students.get(id)['gender']:<10}"
-        )
-    else:
-        print("No id")
+
 
 
 def update_student_details(student_id):
@@ -197,7 +203,7 @@ def update_student_details(student_id):
 def delete_student(id):
     global overwrite
     overwrite = True
-    if students.get(id) != None:
+    if students.get(id):
         students.pop(id)
         print(f"student with ID={id} deleted")
     else:
@@ -246,6 +252,7 @@ def load():
                     data_dict[key.strip("\n")] = value.strip("\n")
                 students[int(st[i])] = data_dict
             print("------- the data has been loaded! -------")
+            
     else:
         print("no data to load")
 
@@ -263,7 +270,7 @@ def get_last_id():
 
 get_last_id()
 while True:
-    print("\nWelcome to Student Database Management System")
+    print("\n<-------- Welcome to Student Database Management System -------->")
     print("What do you want to do? ")
     print("1 - Add Student ")
     print("2 - View Students")
@@ -275,7 +282,8 @@ while True:
     print("press (-1) to Exit")
 
     try:
-        choice = int(input("Enter your choice:"))
+        choice = int(input("Enter your choice: "))
+        
         if choice == -1:
             break
 
@@ -288,7 +296,8 @@ while True:
             print("------------------------------------")
 
         elif choice == 3:
-            id = int(input("Enter the ID you want to Search about:"))
+
+            id = int(input("Enter the ID you want to Search about: "))
             search_student(id)
 
         elif choice == 4:
@@ -300,7 +309,9 @@ while True:
                 print("Student not found.")
 
         elif choice == 5:
-            your_id = int(input("Enter id you want to delete?"))
+
+            your_id = int(input("Enter id you want to delete? "))
+
             delete_student(your_id)
 
         elif choice == 6:
